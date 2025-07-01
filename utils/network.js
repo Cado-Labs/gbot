195817d6d205a20b686bff6074e2732951bc1905
alexander.baz <alexander.baz@cadolabs.io>
2025-07-01 14:07:17 +0300

    migrate to axios

--- utils/network.js
+++ utils/network.js
@@ -1,84 +1,52 @@
-const http = require("http")
-const https = require("https")
-const qs = require("querystring")
-const url = require("url")
+const axios = require("axios")
 
 const logger = require("./logger")
 const { NetworkError } = require("./errors")
 
-const getEngine = uri => {
-  return uri.protocol === "http:" ? http : https
-}
+axios.interceptors.request.use(request => {
+  const { url, data } = request
+  const method = request.method.toUpperCase()
 
-const isErrorStatus = status => status >= 400
-
-const getErrorMessage = ({ response, status, uri }) => {
-  const message = response || `${status} Network Error`
-  return `Got '${message}' message for '${uri}' request`
-}
-
-const get = (to, params = {}, headers = {}) => new Promise((resolve, reject) => {
-  const uri = new url.URL(to)
-  const engine = getEngine(uri)
-  const query = qs.stringify(params)
-  const endpoint = query ? `${to}?${query}` : to
-
-  logger.debug(`GET ${endpoint}`)
-
-  engine.get(endpoint, { headers }, resp => {
-    let data = ""
-
-    resp.on("data", chunk => (data += chunk))
-    resp.on("end", () => {
-      const json = JSON.parse(data)
-
-      if (isErrorStatus(resp.statusCode)) {
-        const errorMessage = getErrorMessage({ response: data, status: resp.statusCode, uri })
-        const error = new NetworkError(errorMessage, resp.statusCode)
-        return reject(error)
-      }
-
-      json.headers = resp.headers
-      resolve(json)
-    })
-  }).on("error", reject)
-})
-
-const post = (to, body, headers = {}) => new Promise((resolve, reject) => {
-  const uri = new url.URL(to)
-  const engine = getEngine(uri)
-  const data = JSON.stringify(body)
-  const request = {
-    host: uri.host,
-    port: uri.port,
-    path: uri.pathname,
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": Buffer.byteLength(data),
-      ...headers,
-    },
+  if (method === "GET") {
+    logger.debug(`${method} ${url}`)
   }
 
-  logger.debug(`POST ${to} ${data}`)
+  if (method === "POST") {
+    logger.debug(`${method} ${url} ${JSON.stringify(data)}`)
+  }
 
-  const req = engine.request(request, resp => {
-    let data = ""
-    resp.on("data", chunk => (data += chunk))
-    resp.on("end", () => {
-      if (isErrorStatus(resp.statusCode)) {
-        const errorMessage = getErrorMessage({ response: data, status: resp.statusCode, uri })
-        const error = new NetworkError(errorMessage, resp.statusCode)
-        return reject(error)
-      }
-
-      resolve(data)
-    })
-  })
-
-  req.on("error", reject)
-  req.write(data)
-  req.end()
+  return request
 })
+
+const handleError = (url, error) => {
+  const status = error.response?.status || 500
+  const response = error.response?.data || error.message
+  const errorMessage = getErrorMessage({ url, status, response })
+  throw new NetworkError(errorMessage, status)
+}
+
+const getErrorMessage = ({ url, status, response }) => {
+  const message = response || `${status} Network Error`
+  return `Got '${message}' message for '${url}' request`
+}
+
+const get = async (url, params = {}, headers = {}) => {
+  try {
+    const resp = await axios.get(url, { headers })
+    return resp.data
+  } catch (error) {
+    handleError(url, error)
+  }
+}
+
+const post = async (url, params, headers = {}) => {
+
+  try {
+    const resp = await axios.post(url, params, { headers })
+    return resp.data
+  } catch (error) {
+    handleError(url, error)
+  }
+}
 
 module.exports = { get, post }
