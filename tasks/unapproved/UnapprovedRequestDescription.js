d4355f0fcbd86c5275af54c76e7aaef3f8958344
Yuri Smirnov <tycoooon@gmail.com>
2026-08-06 16:47:23 +0300

    Use GitLab display name when no messenger mapping is configured (#12)
    * Add displayName helper for GitLab users
    
    * Pass GitLab user object into markup mention
    
    * Resolve author display names from GitLab when unmapped
    
    * Document display name fallback for unmapped users
    
    * Bump version to 2.6.0

--- tasks/unapproved/UnapprovedRequestDescription.js
+++ tasks/unapproved/UnapprovedRequestDescription.js
@@ -3,6 +3,7 @@ const gitUtils = require("../../utils/git")
 const timeUtils = require("../../utils/time")
 const stringUtils = require("../../utils/strings")
 const markupUtils = require("../../utils/markup")
+const userUtils = require("../../utils/users")
 
 class UnapprovedRequestDescription {
   constructor (type, request, config) {
@@ -21,7 +22,7 @@ class UnapprovedRequestDescription {
     const projectLink = markup.makeLink(this.request.project.name, this.request.project.web_url)
     const unresolvedAuthors = this.__unresolvedAuthorsString(markup)
     const authorString = this.__authorString(
-      markup, author.username, { tag: this.__tagAuthorInPrimaryMessage() },
+      markup, author, { tag: this.__tagAuthorInPrimaryMessage() },
     )
     const approvedBy = this.__approvedByString(markup)
 
@@ -56,7 +57,7 @@ class UnapprovedRequestDescription {
     }
 
     if (this.__hasConflicts()) {
-      const authorString = this.__authorString(markup, author.username, { tag: this.__tagOnConflict() })
+      const authorString = this.__authorString(markup, author, { tag: this.__tagOnConflict() })
       const text = `conflicts: ${authorString}`
       const msg = markup.makeText(text, { withMentions: this.__tagOnConflict() })
 
@@ -64,7 +65,7 @@ class UnapprovedRequestDescription {
     }
 
     if (this.__isPipelineFailed()) {
-      const authorString = this.__authorString(markup, author.username, { tag: this.__tagOnFailedPipeline() })
+      const authorString = this.__authorString(markup, author, { tag: this.__tagOnFailedPipeline() })
       const text = `pipeline failed: ${authorString}`
       const msg = markup.makeText(text, { withMentions: this.__tagOnFailedPipeline() })
 
@@ -100,7 +101,7 @@ class UnapprovedRequestDescription {
 
   __unresolvedAuthorsString = (markup) => {
     return this.__unresolvedAuthorsFor(this.request).map(author => (
-      this.__authorString(markup, author.username, { tag: true })
+      this.__authorString(markup, author, { tag: true })
     )).join(", ")
   }
 
@@ -108,21 +109,21 @@ class UnapprovedRequestDescription {
     const tag = this.__getConfigSetting("unapproved.tag.approvers", false)
 
     return this.request.approved_by.map(approve => (
-      this.__authorString(markup, approve.user.username, { tag })
+      this.__authorString(markup, approve.user, { tag })
     )).join(", ")
   }
 
-  __authorString = (markup, username, { tag = false } = {}) => {
+  __authorString = (markup, user, { tag = false } = {}) => {
     if (tag) {
-      return this.__getMentionString(markup, username)
+      return this.__getMentionString(markup, user)
     }
 
-    return stringUtils.wrapString(`@${username}`)
+    return stringUtils.wrapString(`@${userUtils.displayName(user)}`)
   }
 
-  __getMentionString = (markup, username) => {
+  __getMentionString = (markup, user) => {
     const mapping = this.__getConfigSetting(`messenger.${markup.type}.usernameMapping`, {})
-    return markup.mention(username, mapping)
+    return markup.mention(user, mapping)
   }
 
   __optionalDiffString = () => {
